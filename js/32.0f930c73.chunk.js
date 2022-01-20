373b45ceb00e86bf7655b9bbabc7a674849908b8
cryptosaurio <cryptosaurio@protonmail.com>
2022-01-20 14:47:55 -0300

    19251ceb3f1639dd8757cf89f52ebfbe608e76f4

---
+++ js/32.0f930c73.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonpfront-in-react"]=this["webpackJsonpfront-in-react"]||[]).push([[32],{711:function(t,r,n){"use strict";n.r(r),n.d(r,"default",(function(){return c}));n(0);var e=n(129),a=n(1);function c(t){var r=t.match,n=Object(e.a)().t;return Object(a.jsx)(a.Fragment,{children:n(r.params.orderBy)})}}}]);
