db312caf90ea8ab0905027cd87f5ece62a1853c8
cryptosaurio <cryptosaurio@protonmail.com>
2022-04-17 12:04:20 -0300

    9d9d6066f7bf5c7d3c2d19412d5895842be7d655

---
+++ js/55.ea3f5e7b.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonpfront-in-react"]=this["webpackJsonpfront-in-react"]||[]).push([[55],{123:function(t,n,a){"use strict";a.r(n),a.d(n,"default",(function(){return c}));a(0);var e=a(138),r=a(2);function c(t){var n=t.match,a=Object(e.a)().t;return Object(r.jsx)(r.Fragment,{children:a("P\xe1gina {{page}}",{page:n.params.page})})}}}]);
